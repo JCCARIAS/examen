22deed30f7b1f3f0b659958ba51ca9becfdeb375
JCCARIAS <107296996+JCCARIAS@users.noreply.github.com>
2024-03-11 22:52:55 -0600

    Add files via upload
    mejora al metodo recursivo para buscar archivos

--- exam/src/ExamProgramacion.java
+++ exam/src/ExamProgramacion.java
@@ -1,46 +1,28 @@
+
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ExamProgramacion {
-    static ArrayList<String> ArrayArchivos = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the directory to search: ");
         String directory = scanner.nextLine();
 
-        System.out.print("Enter the name of the file: ");
-        String nameFile = scanner.nextLine();
-
-        encontrarArchivo(new File(directory), nameFile);
-
-        if (ArrayArchivos.isEmpty()) {
-            System.out.println("File not found.");
-        } else {
-            System.out.println("File  found:");
-            for (String fileName : ArrayArchivos) {
-                System.out.println(fileName);
-            }
-        }
+        encontrarDirectorio(new File(directory));
     }
 
-    public static void encontrarArchivo(File directory, String nameFile) {
-        File[] folders = directory.listFiles();
+    public static void encontrarDirectorio(File directory) {
+        if (directory.isDirectory()) {
+            System.out.println("Directorio: " + directory.getName());
 
-        if (folders != null) {
-            for (File folder : folders) {
-                if (folder.isDirectory()) {
-                    encontrarArchivo(folder, nameFile);
-                } else { 
-                    if (folder.getName().equals(nameFile) || folder.getName().endsWith(".pnd") || folder.getName().endsWith(".docx") ) {
-                        ArrayArchivos.add(folder.getAbsolutePath());
-                    }
+            File[] archivos = directory.listFiles();
+
+            if (archivos != null) {
+                for (File archivo : archivos) {
+                    encontrarDirectorio(archivo);
                 }
             }
         }
     }
 }
-    	 
-   
